5f5618013cff870bad60527e1bd599d79f61fdae
im <im@im.im>
2018-07-11 14:21:16 +0300

    after commenting

--- power.cpp
+++ power.cpp
@@ -1,5 +1,6 @@
+/*! \file */ 
 #include <iostream>
-
+//! this function powered number
 template<typename T> T MyPow(T Base, T PowerBase){
     T Result = 1;
     for (int j = 1; j <= PowerBase; j++){
@@ -7,12 +8,16 @@ template<typename T> T MyPow(T Base, T PowerBase){
     }
     return Result;
 }
+//! this function test
 void Test(){
     for (int i = 0; i < 11; i++){
         std::cout << MyPow(2, i) << std::endl;
     }
 }
-
+//! main function
+/*
+ specify  of main
+*/
 int main()
 {
     Test();
